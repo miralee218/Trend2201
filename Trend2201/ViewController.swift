d53302a095485184fb4a2e411987768287e5de44
Mira <applesharonme@gmail.com>
2019-06-10 14:34:46 +0800

    git init for project

---
+++ Trend2201/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Trend2201
+//
+//  Created by Mira on 2019/6/10.
+//  Copyright © 2019 AppWork. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
